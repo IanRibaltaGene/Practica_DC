aaf82ada18a377f118bb0c3e06a03427ead0ce2c
IanRibaltaGene <ian.ribalta@gmail.com>
2022-11-28 14:03:29 +0100

    Topics autocreate and info send to analyics module (wrong type of data "TypeError: byte indices must be integers or slices, not str")

--- cloud/subscribe.py
+++ cloud/subscribe.py
@@ -1,11 +1,14 @@
 import paho.mqtt.subscribe as subscribe
-from time import sleep
+from kafka import KafkaProducer
+from pickle import dumps
+
+kafka_producer = KafkaProducer(    
+    bootstrap_servers=['kafka : 29092'],
+    value_serializer=lambda x: dumps(x))
 
 def on_message(client, userdata, message):
     print("%s %s" % (message.topic, message.payload))
+    kafka_producer.send('analytics', value=message.payload)
+    #Store to influxDB
 
-try:
-    subscribe.callback(on_message, "Gateway/+/temperature", hostname="host.docker.internal")
-except: ##If not able to subscribe, wait for broker and retry
-    sleep(2)
-    subscribe.callback(on_message, "Gateway/+/temperature", hostname="host.docker.internal")
+subscribe.callback(on_message, "Gateway/+/temperature", hostname="host.docker.internal")
